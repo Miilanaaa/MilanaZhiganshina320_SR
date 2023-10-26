79d3300cb806b53e18f90ae2bed0b30c5aec096c
Милана <@LAPTOP-T2ETA51T>
2023-10-26 08:51:52 +0300

    Добавьте файлы проекта.

---
+++ MilanaZhiganshina320_SR/DB/DbConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilanaZhiganshina320_SR.DB
+{
+    internal class DbConnection
+    {
+        public static PM_SREntities PM_SREntities = new PM_SREntities();
+    }
+}
---
+++ MilanaZhiganshina320_SR/Pages/AuthorizationPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using MilanaZhiganshina320_SR.DB;
+using System.Windows.Shapes;
+
+
+namespace MilanaZhiganshina320_SR.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthorizationPage.xaml
+    /// </summary>
+    public partial class AuthorizationPage : Page
+    {
+        public static List<Role> roles { get; set; }
+        public AuthorizationPage()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string login = loginTb.Text.Trim();
+            string password = passwordTb.Password.Trim();
+
+            roles = new List<Role>(DbConnection.PM_SREntities.Role.ToList());
+            Role currentUser = roles.FirstOrDefault(i => i.Login == login && i.Password == password);
+            if (currentUser != null)
+                NavigationService.Navigate(new GlavPage());
+            else
+                MessageBox.Show("Неверные данные");
+
+        }
+    }
+}
